636cd24a5f85cd411f7af2a5849cfb1ef485d7c8
s3mPr1linux <rodrigosilvaluz3@gmail.com>
2023-12-16 18:49:38 -0300

    Commit Session

--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+"""
+Hello World Multi Linguas.
+
+Dependendo da ling configurada no ambiente, 
+o programa exibe a mensagem correspondente.
+
+Como usar:
+
+    Tenha a variavel LANG devidamente configurada ex:
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+
+"""
+__version__ = "0.0.1"
+__autor__   = "SL"
+__license__ = "Unlicense"
 
 print("Hello, World!")
 
